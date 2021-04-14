998460c103c98f9f559795f8907e46709466767e
svilenaa <svilenaa96@gmail.com>
2021-04-14 19:19:34 +0300

    commit

---
+++ www/js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    loadStudents();
+    $("#camera-btn").click(getPicutre);
+    $("#addPicture").click(sendPictureRequest);
+
+}
